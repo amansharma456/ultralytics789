2c6aa05a78e08d1ac3d43f7531fd774dae48ef73
Aman Sharma <amansharma18372@gmail.com>
2026-06-19 23:04:01 +0530

    Update tasks.py
    Signed-off-by: Aman Sharma <amansharma18372@gmail.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -916,8 +916,8 @@ class RTDETRDetectionModel(DetectionModel):
                 x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]  # from earlier layers
             if profile:
                 self._profile_one_layer(m, x, dt)
-            x = m(x)  # run
-           y.append(x if m.i in self.save else None)  # save output
+                x = m(x)  # run
+                y.append(x if m.i in self.save else None)  
             if visualize:
                 feature_visualization(x, m.type, m.i, save_dir=visualize)
             if m.i in embed:
