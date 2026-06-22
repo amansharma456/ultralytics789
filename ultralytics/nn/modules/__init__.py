303d2aabf8f197bc51e88f6caa4c1b3828db919b
Aman Sharma <amansharma18372@gmail.com>
2026-06-22 19:33:11 +0530

    Update __init__.py
    Signed-off-by: Aman Sharma <amansharma18372@gmail.com>

--- ultralytics/nn/modules/__init__.py
+++ ultralytics/nn/modules/__init__.py
@@ -16,7 +16,7 @@ Examples:
     >>> torch.onnx.export(m, x, f)
     >>> subprocess.run(f"onnxslim {f} {f} && open {f}", shell=True, check=True)  # pip install onnxslim
 """
-from .fno_backbone import FNOBackbone
+from .convnextv2 import ConvNeXtV2Backbone
 from .block import (
     C1,
     C2,
